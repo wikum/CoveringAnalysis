aa1fe79e7dcf904181d17173d092b6d6de447252
Wikum <wikumdina@gmail.com>
2020-11-13 20:25:37 -0500

    Updates

--- R/5_HETEROGENEITY/run.R
+++ R/5_HETEROGENEITY/run.R
@@ -1,24 +1,10 @@
 
 
-if(! dir.exists("obj"))
-  dir.create("obj")
-
-if(! dir.exists("log"))
-  dir.create("log")
-
 tryCatch({
-  cat("\n RNA-Seq NORMAL vs. TUMOR \n ===================== \n")
-  source("1_1.R")
+  cat("\n HETEROGENEITY ANALYSIS \n ===================== \n")
+
 }, error = function(e){print(e)})
 
-tryCatch({
-  cat("\n Methylation NORMAL vs. TUMOR \n ===================== \n")
-  source("1_2.R")
-}, error = function(e){print(e)})
 
-tryCatch({
-  cat("\n Microarray NORMAL vs. TUMOR \n ===================== \n")
-  source("1_3.R")
-}, error = function(e){print(e)})
 
 
---
+++ run_all.R
@@ -0,0 +1,27 @@
+
+
+
+cat("--------------------------------------\n")
+setwd("R/1_PREPROCESS/")
+  source("run.R")
+setwd("../../")
+
+cat("--------------------------------------\n")
+setwd("R/2_COVER/")
+  source("run.R")
+setwd("../../")
+
+cat("--------------------------------------\n")
+setwd("R/3_ANALYSIS/")
+  source("run.R")
+setwd("../../")
+
+cat("--------------------------------------\n")
+setwd("R/4_SUBTYPES/")
+  source("run.R")
+setwd("../../")
+
+cat("--------------------------------------\n")
+setwd("R/5_HETEROGENEITY/")
+  source("run.R")
+setwd("../../")
